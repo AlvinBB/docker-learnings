ae55e78690451a606576a756fb28e098fc0446f5
jiangyunbin <jiangyunbin@baidu.com>
2019-08-17 23:00:21 +0800

    update version test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 1.0.0
+          Learn React 1.0.1
         </a>
       </header>
     </div>
